08ecf8b0b5fb797bcb8e5a061ac7c2f2285d3b80
arnetkc <arnetkc@gmail.com>
2024-07-15 10:46:35 +0530

    FEAT : add checkout, address , thanks page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
 import Checkout from "./components/Checkout";
+import Thanks from "./components/Thanks";
 
 export const config = {
-  // endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
-  endpoint: `https://qkart-97sz.onrender.com/api/v1`,
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  // endpoint: `https://qkart-97sz.onrender.com/api/v1`,
 };
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <Route path="/checkout" component={Checkout} />
+        <Route path="/thanks" component={Thanks} />
       </Switch>
     </div>
   );
