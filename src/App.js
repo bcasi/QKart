876cc7786db247c0cf699916d5abf12a4be8b10f
arnetkc <arnetkc@gmail.com>
2024-07-20 14:38:59 +0530

    Fix: css box-sizing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  // endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
-  endpoint: `https://qkart-97sz.onrender.com/api/v1`,
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  // endpoint: `https://qkart-97sz.onrender.com/api/v1`,
 };
 
 function App() {
